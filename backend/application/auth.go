7ffa2a86f58c39c0a558618317166e1de73ec856
shjhwoo <108392916+shjhwoo@users.noreply.github.com>
2022-10-07 18:35:42 +0900

    logout

---
+++ backend/application/auth.go
@@ -0,0 +1,93 @@
+package application
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+)
+
+type cookie struct {
+	Cookie string `json:"cookie"`
+}
+
+//acctoken과 같이 들어온 요청이 유효한지를 판단하는 미들웨어
+func checkAcctoken(c *gin.Context){
+	fmt.Println("미들웨어: 베가스 쿠키가 유효한지 검증합니다")
+	var rw http.ResponseWriter = c.Writer
+	var req *http.Request = c.Request
+
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("요청 바디 자체를 못 읽었음")
+	}
+
+	var vegasCookie cookie
+	if err := json.Unmarshal(data,&vegasCookie); err != nil {
+		fmt.Println(err,"제이쓴 파싱에 실패했습니다")
+	}
+
+	if vegasCookie.Cookie == "" || vegasCookie.Cookie == "isPKCE=true" {
+		fmt.Println(err,"베가스 쿠키가 없습니다")
+		con := oauth2.Config{
+			ClientID: "vegas",
+			ClientSecret: "foobar",
+			RedirectURL: "http://localhost:3006/callback",
+			Scopes: []string{"openid", "offline"},
+			Endpoint: oauth2.Endpoint{
+				TokenURL: "http://localhost:8080/api/oauth2/token",
+				AuthURL:  "http://localhost:8080/",
+			},
+		}
+
+		pkceCodeVerifier := generateCodeVerifier(64)
+		pkceCodeChallenge = generateCodeChallenge(pkceCodeVerifier)
+		//1.sso통합 로그인 페이지 생성
+		ssoLoginURL := con.AuthCodeURL("nuclear-tuna-plays-piano")+"&nonce=some-random-nonce&code_challenge="+pkceCodeChallenge+"&code_challenge_method=S256"
+		//이 주소를 다시 프론트로 보내서 리디렉션 시켜줌
+		fmt.Println(err,"SSO 쿠키가 있는지 확인하러 갑니다...")
+		c.JSON(http.StatusOK, gin.H{
+			"redirectionURL": ssoLoginURL,
+		})
+		return
+	}
+
+	//쿠키가 존재. jwt 파싱해서 유효한 토큰인지를 확인
+	fmt.Println("액세스 토큰을 파싱하여 검증합니다")
+	var jwtKey = []byte("sercrethatmaycontainch@r$32chars")
+	tknStr := strings.Split(vegasCookie.Cookie,"=")[1]
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			rw.WriteHeader(http.StatusUnauthorized)
+			fmt.Println("권한 없는 이상한 토큰입니다.")
+			return
+		}
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Println("아무튼 잘못된 요청입니다.")
+		return
+	}
+
+	if !tkn.Valid {
+		rw.WriteHeader(http.StatusUnauthorized)
+		fmt.Println("권한이 없는 요청입니다.")
+		return
+	}
+
+	fmt.Println("유효한 토큰입니다. api에 요청을 보냅니다...")
+	c.Next()
+
+	// cookie, err := req.Cookie("vegasAccessToken")
+	// if err != nil {
+	// 	fmt.Println("쿠키를 찾을 수 없습니다. SSO 쿠키가 있는지 확인하러 갑니다.")
+	// }
+}
---
+++ backend/application/logout.go
@@ -0,0 +1,90 @@
+package application
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+)
+
+func logoutHandler(c *gin.Context) {
+	fmt.Println("로그아웃 요청이 들어왔습니다")
+	var rw http.ResponseWriter = c.Writer
+	var req *http.Request = c.Request
+	
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("요청 바디 자체를 확인할 수 없습니다")
+		return
+	}
+	var cookie cookie
+	if err := json.Unmarshal(data, &cookie); err != nil {
+		fmt.Println("제이슨 형식으로 쿠키 파싱하는 데 실패함")
+		return
+	}
+
+	fmt.Println(cookie.Cookie,"쿠키 값 확인하세요")
+
+	cookiename := strings.Split(cookie.Cookie, "=")[0]
+	if cookiename != "vegasAccessToken" {
+		fmt.Println("베가스 서비스 쿠키가 아닙니다.")
+		return
+	}
+
+	tknStr := strings.Split(cookie.Cookie, "=")[1]
+
+	fmt.Println("유효한 토큰인지 검증 시작합니다...")
+	claims := &Claims{}
+	var jwtKey = []byte("sercrethatmaycontainch@r$32chars")
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			rw.WriteHeader(http.StatusUnauthorized)
+			fmt.Println("권한 없는 이상한 토큰")
+			return
+		}
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Println("아무튼 잘못된 요청")
+		return
+	}
+
+	if !tkn.Valid {
+		rw.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("받은 쿠키는 유효합니다. 쿠키를 파괴합니다")
+	c.SetCookie("vegasAccessToken","",0,"/","localhost",false,false)
+
+	//그런 다음에 SSO 세션을 체크하는 url을 사용자에게 되돌려준다.
+	//sso 쿠키가 살아있는지 확인해야지...
+	con := oauth2.Config{
+		ClientID: "vegas",
+		ClientSecret: "foobar",
+		RedirectURL: "http://localhost:3006/callback",
+		Scopes: []string{"openid", "offline"},
+		Endpoint: oauth2.Endpoint{
+			TokenURL: "http://localhost:8080/api/oauth2/token",
+			AuthURL:  "http://localhost:8080/",
+		},
+	}
+
+	pkceCodeVerifier := generateCodeVerifier(64)
+	pkceCodeChallenge = generateCodeChallenge(pkceCodeVerifier)
+
+	//1.sso통합 로그인 페이지 생성
+	ssoLoginURL := con.AuthCodeURL("nuclear-tuna-plays-piano")+"&nonce=some-random-nonce&code_challenge="+pkceCodeChallenge+"&code_challenge_method=S256&logout=true"
+	//이 주소를 다시 프론트로 보내서 리디렉션 시켜줌
+	c.JSON(http.StatusOK, gin.H{
+		"redirectionURL": ssoLoginURL,
+	})
+}
