d743d6385d88b24e25da4644690e8d99fefe3d2e
simon <simon@simonui-MacBookAir.local>
2022-10-12 13:44:12 +0900

    slo

--- backend/application/ssologin.go
+++ backend/application/ssologin.go
@@ -8,17 +8,17 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func ssologinHandler(c *gin.Context){
+func ssologinHandler(c *gin.Context) {
 	var w http.ResponseWriter = c.Writer
 	var r *http.Request = c.Request
 	fmt.Println("요청확인")
 	//클라이언트 정보에 따라 통합 로그인 페이지 접속 url을 만들어 준 후 그쪽으로 사용자를 보내준다.
 	//0.클라이언트 설정
 	con := oauth2.Config{
-		ClientID: "vegas",
+		ClientID:     "vegas",
 		ClientSecret: "foobar",
-		RedirectURL: "http://localhost:3006/callback",
-		Scopes: []string{"openid", "offline"},
+		RedirectURL:  "http://localhost:3006/callback",
+		Scopes:       []string{"openid", "offline"},
 		Endpoint: oauth2.Endpoint{
 			TokenURL: "http://localhost:8080/api/oauth2/token",
 			AuthURL:  "http://localhost:8080/",
@@ -29,9 +29,9 @@ func ssologinHandler(c *gin.Context){
 	pkceCodeChallenge = generateCodeChallenge(pkceCodeVerifier)
 
 	//1.sso통합 로그인 페이지 생성
-	ssoLoginURL := con.AuthCodeURL("nuclear-tuna-plays-piano")+"&nonce=some-random-nonce&code_challenge="+pkceCodeChallenge+"&code_challenge_method=S256"
+	ssoLoginURL := con.AuthCodeURL("nuclear-tuna-plays-piano") + "&nonce=some-random-nonce&code_challenge=" + pkceCodeChallenge + "&code_challenge_method=S256"
 
-	fmt.Println(ssoLoginURL,"요청url")
+	fmt.Println(ssoLoginURL, "요청url")
 	//2.사용자 리디렉션
 	http.Redirect(w, r, ssoLoginURL, http.StatusSeeOther)
 }
